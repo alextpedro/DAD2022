b4f720e5ba5174959d345e07f784cae9cc11d3b2
Alex Pedro <alextpedro@gmail.com>
2022-11-21 21:29:37 +0000

    Adds logout functionality.

--- fastuga_frontend/src/App.vue
+++ fastuga_frontend/src/App.vue
@@ -14,6 +14,17 @@ const clickMenuOption = () => {
     buttonSidebarExpand.value.click()
   }
 }
+
+const logout = async () => {
+  if (await userStore.logout()) {
+    toast.success("User has logged out of the application.")
+    clickMenuOption()
+    router.push({name: 'home'})
+  } else {
+    toast.error("There was a problem logging out of the application!")
+  }
+}
+
 </script>
 
 <template>
