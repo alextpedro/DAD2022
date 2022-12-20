4120efae821aba158e86390ba669c8e6dad45af1
Alex Pedro <alextpedro@gmail.com>
2022-12-20 01:48:52 +0000

    Improves application UI handling of Users.

--- fastuga_frontend/src/App.vue
+++ fastuga_frontend/src/App.vue
@@ -42,39 +42,44 @@ const logout = async () => {
 
 			<div class="collapse navbar-collapse justify-content-end">
 				<ul class="navbar-nav">
-					<li class="nav-item">
-						<router-link class="nav-link pr-3" to="/register">Register</router-link>
+					<li class="nav-item" v-show="!userStore.user">
+						<router-link class="nav-link pr-3" to="/register">
+							<i class="bi bi-person-check-fill"></i>
+							Register
+						</router-link>
 					</li>
-					<li class="nav-item">
+					<li class="nav-item" v-show="!userStore.user">
 						<router-link class="nav-link" :class="{ active: $route.name === 'Login' }"
 							:to="{ name: 'Login' }">
 							<i class="bi bi-box-arrow-in-right"></i>
 							Login
 						</router-link>
 					</li>
-					<li class="nav-item dropdown">
+					<li class="nav-item dropdown" v-show="userStore.user">
 						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
 							data-bs-toggle="dropdown" aria-expanded="false">
-							<img src="@/assets/avatar-exemplo-1.jpg" class="rounded-circle z-depth-0 avatar-img"
+							<img
+								:src="userStore.userPhotoUrl"
+								class="rounded-circle z-depth-0 avatar-img"
 								alt="avatar image" />
-							<span class="avatar-text">User Name</span>
+								<span class="avatar-text">{{ userStore.user?.name ?? "Anonymous" }}</span>
 						</a>
 						<ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end"
 							aria-labelledby="navbarDropdownMenuLink">
-							<!-- <li>
-								<router-link class="dropdown-item"
-									:class="{ active: $route.name == 'User' && $route.params.id == 1 }"
-									:to="{ name: 'User', params: { id: 1 } }">
-									<i class="bi bi-person-square"></i>Profile
+							<li>
+								<router-link class="dropdown-item" :class="{ active: $route.name == 'User'}"
+									:to="{ name: 'EditUser' }" @click="clickMenuOption">
+									<i class="bi bi-person-square"></i>
+									Profile
 								</router-link>
 							</li>
 							<li>
 								<router-link class="dropdown-item" :class="{ active: $route.name === 'ChangePassword' }"
-									:to="{ name: 'ChangePassword' }">
+									:to="{ name: 'Home' }">
 									<i class="bi bi-key-fill"></i>
 									Change password
 								</router-link>
-							</li> -->
+							</li>
 							<li>
 								<hr class="dropdown-divider" />
 							</li>
@@ -82,7 +87,7 @@ const logout = async () => {
 								<a class="dropdown-item" @click.prevent="logout">
 									<i class="bi bi-arrow-right"></i>Logout
 								</a>
-							</li>
+							</li> 
 						</ul>
 					</li>
 				</ul>
@@ -103,48 +108,6 @@ const logout = async () => {
 								Menu
 							</router-link>
 						</li>
-						<li class="nav-item">
-							<!-- <router-link
-                class="nav-link"
-                :class="{ active: $route.name === 'CurrentTasks' }"
-                :to="{ name: 'CurrentTasks' }"
-              >
-                <i class="bi bi-list-stars"></i>
-                Current Tasks
-              </router-link> -->
-						</li>
-						<li class="nav-item d-flex justify-content-between align-items-center pe-3">
-							<!-- <router-link
-                class="nav-link w-100 me-3"
-                :class="{ active: $route.name === 'Tasks' }"
-                :to="{ name: 'Tasks' }"
-              >
-                <i class="bi bi-list-check"></i>
-                Tasks
-              </router-link>
-              <router-link class="link-secondary" :to="{ name: 'NewTask'}" aria-label="Add a new task">
-                <i class="bi bi-xs bi-plus-circle"></i>
-              </router-link> -->
-						</li>
-						<li class="nav-item">
-							<!-- <router-link
-                class="nav-link"
-                :class="{ active: $route.name === 'Projects' }"
-                :to="{ name: 'Projects' }"
-              >
-                <i class="bi bi-files"></i>
-                Projects
-              </router-link> -->
-						</li>
-						<!-- <li class="nav-item"> -->
-							<!-- <router-link
-                class="nav-link"
-                :class="{ active: $route.name === 'Users' }"
-                :to="{ name: 'Users' }"
-              >
-                <i class="bi bi-people"></i>
-                Team Members
-              </router-link> -->
 						
 						<li class="nav-item">
 							<!-- <router-link class="nav-link bi bi-person" to="/register">Register</router-link> -->
@@ -161,19 +124,19 @@ const logout = async () => {
 						
 					</h6>
 					<ul class="nav flex-column mb-2">
-						<li class="nav-item">
+						<li class="nav-item" v-if="userStore.user?.type === 'EM'">
 							<router-link class="nav-link w-100 me-3" :to="{ name: 'Plataformstatistics'}" aria-label="See platform statistics.">
 								<i class="bi bi-bar-chart-line">Statistics</i>
 							</router-link>
 						</li>
-						<!--<li class="nav-item" v-for="prj in workInProgressProjects" :key="prj.id">
-							<router-link class="nav-link w-100 me-3" 
-                :class="{active: $route.name == 'ProjectTasks' && $route.params.id == prj.id}"
-                :to="{ name: 'ProjectTasks', params: { id: prj.id }}">
-                <i class="bi bi-file-ruled"></i>
-                {{ prj.name}}
-              </router-link> 
-						</li>-->
+						
+						<li class="nav-item" v-if="userStore.user?.type === 'EC'">
+							<router-link class="nav-link" :class="{ active: $route.name === 'Chefs' }"
+								:to="{ name: 'Chefs' }" >
+								<i class="bi bi-list-stars"></i>
+								Chefs Hot Dishes
+							</router-link>
+						</li>
 					</ul>
 
 					<div class="d-block d-md-none">
--- fastuga_frontend/src/stores/user.js
+++ fastuga_frontend/src/stores/user.js
@@ -5,53 +5,54 @@ import avatarNoneUrl from '@/assets/avatar-none.png';
 export const useUserStore = defineStore('user', () => {
 	const axios = inject('axios');
 	const serverBaseUrl = inject('serverBaseUrl');
+	const apiPort = inject('apiPort');
 	const socket = inject('socket');
-    
+
 	const user = ref(null);
-    
+
 	const userPhotoUrl = computed(() => {
 		if (!user.value?.photo_url) {
 			return avatarNoneUrl;
 		}
-		return serverBaseUrl + '/storage/fotos/' + user.value.photo_url;
+		return serverBaseUrl + apiPort + '/storage/fotos/' + user.value.photo_url;
 	});
-    
+
 	const userId = computed(() => {
 		return user.value?.id ?? -1;
 	});
 
-	async function loadUser () {
+	async function loadUser() {
 		try {
 			const response = await axios.get('users/me');
 			user.value = response.data.data;
 		} catch (error) {
-			clearUser (); 
+			clearUser();
 			throw error;
 		}
 	}
-    
-	function clearUser () {
+
+	function clearUser() {
 		delete axios.defaults.headers.common.Authorization;
 		sessionStorage.removeItem('token');
 		user.value = null;
-	}  
-    
-	async function login (credentials) {
+	}
+
+	async function login(credentials) {
 		try {
 			const response = await axios.post('login', credentials);
 			axios.defaults.headers.common.Authorization = 'Bearer ' + response.data.access_token;
 			sessionStorage.setItem('token', response.data.access_token);
 			await loadUser();
 			socket.emit('loggedIn', user.value);
-			return true;       
-		} 
-		catch(error) {
+			return true;
+		}
+		catch (error) {
 			clearUser();
 			return false;
 		}
 	}
-    
-	async function logout () {
+
+	async function logout() {
 		try {
 			await axios.post('logout');
 			socket.emit('loggedOut', user.value);
@@ -62,7 +63,7 @@ export const useUserStore = defineStore('user', () => {
 		}
 	}
 
-	async function restoreToken () {
+	async function restoreToken() {
 		let storedToken = sessionStorage.getItem('token');
 		if (storedToken) {
 			axios.defaults.headers.common.Authorization = 'Bearer ' + storedToken;
@@ -73,6 +74,6 @@ export const useUserStore = defineStore('user', () => {
 		clearUser();
 		return false;
 	}
-    
+
 	return { user, userId, userPhotoUrl, login, logout, restoreToken };
 });
